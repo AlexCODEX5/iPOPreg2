ea06b5cc881de77420773747c118ff345f73d82d
AlexCODEX5 <32914572+AlexCODEX5@users.noreply.github.com>
2019-11-01 21:51:08 -0500

    Subida de archivos Principales

---
+++ iPOPreg/BDasistente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace iPOPreg
+{
+    class BDasistente
+    {
+        private string conectionString;
+        private string table;
+        private string query;
+
+        public string CadenaConexion()
+        {
+            return conectionString;
+        }
+
+        public void SetCadenaConexion(string newCadena)
+        {
+            conectionString = newCadena;
+        }
+        
+        public string CadenaTable()
+        {
+            return table;
+        }
+
+        public void SetCadenaTable(string newTable)
+        {
+            table = newTable;
+        }
+
+        public string CadenaQuery()
+        {
+            return query;
+        }
+
+    }
+}
---
+++ iPOPreg/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace iPOPreg
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Salir_Login_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Min_Login_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Verificar_Login_Click(object sender, RoutedEventArgs e)
+        {
+            Panel_control ingreso = new Panel_control();
+            ingreso.Show();
+            this.Close();
+        }
+
+        private void BordeTit_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void Panel_Login_Loaded(object sender, RoutedEventArgs e)
+        {
+            User_Login.Focus();
+        }
+    }
+}
